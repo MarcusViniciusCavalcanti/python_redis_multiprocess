7084fc5581fe0c23909cbb3af7d1abb8fe5318f1
vinicius <vinicius@pcs-MacBook-Pro-2.local>
2019-08-27 21:49:57 -0300

    complete

--- redis_modules.py
+++ redis_modules.py
@@ -1,5 +1,20 @@
-from time import sleep
+import random
 
-def print_as_function(argument):
-  sleep(2)
-  return argument
+def cria_matriz(linhas, colunas):
+  A = []
+  for i in range(linhas):
+    linha = []
+    for j in range(colunas):
+      linha = linha + [random.randint(1, 10)]
+    A = A + [linha]
+  return A
+
+def multiplica_linha_coluna(matrizA, matrizB):
+  valor = 0
+
+  print(matrizB)
+  for k in range(len(matrizB)):
+    b_ = matrizB[k]
+    print(b_)
+    valor = valor + matrizA[k] * b_
+  return valor
